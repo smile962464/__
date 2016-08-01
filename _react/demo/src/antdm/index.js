bd5e4e8c95ea3fc92536d7499f757a0a657b9640
然则 <hualei.hl@alibaba-inc.com>
2016-08-01 22:20:00 +0800

    autocommit

--- _react/demo/src/antdm/index.js
+++ _react/demo/src/antdm/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 // import 'antd-mobile/lib/button/style';
 // import Button from 'antd-mobile/lib/button';
 
 // import js and css modularly, parsed by babel-plugin-antd
 import { Button } from 'antd-mobile';
 
+// import pc antd
+// import { ButtonPc } from 'antd';
+import 'antd/lib/button/style';
+import ButtonPc from 'antd/lib/button';
+
 const App = React.createClass({
   render() {
     const items = [{id: '1'}, {id: 'header'}];
     return (
       <div style={{ margin: 10 }}>
-        <Button onClick={(e) => console.log(e)}>Start</Button>
+        <Button onClick={(e) => console.log(e) }>Start</Button> <br />
+        <ButtonPc onClick={(e) => console.log(e) }>Start</ButtonPc>
       </div>
     );
   }
