8b36e4f66c4d8250f7bafe027f23288dbd19e2a6
然则 <hualei.hl@alibaba-inc.com>
2016-10-23 23:36:50 +0800

    autocommit

---
+++ misc/node/_server/readme.md
@@ -0,0 +1,10 @@
+## local server
+> port 9998
+
+    forever start server.js
+    forever stop server.js
+
+[http://localhost:9998/](http://localhost:9998/) routers: 
+
+- [joke=1](http://localhost:9998/?joke=1)
+- [joke=2](http://localhost:9998/?joke=2)
---
+++ misc/node/_server/server.js
@@ -0,0 +1,70 @@
+var http = require('http');
+var url = require('url');
+var fs = require('fs-extra');
+var path = require('path');
+var static = require('node-static');
+var probe = require('probe-image-size');
+
+var port = 9998;
+var jsonContentType = 'application/json; charset=utf-8';
+
+var imgExts = ['jpg', 'jpeg', 'gif', 'png'];
+var local1 = '/Users/hua/Downloads/_/百度云同步盘/2008-now.txt';
+var local2 = '/Users/hua/Downloads/_/百度云同步盘/图片收集';
+
+function handleJoke1(res) {
+  var content = fs.readFileSync(local1).toString().split('\n\n');
+  res.writeHead(200, {'Content-Type': jsonContentType});
+  res.end(JSON.stringify(content));
+}
+function handleJoke2(res) {
+  var items = [];
+  fs.walk(local2).on('data', function (item) {
+    var fileDir = item.path;
+    var extname = path.extname(fileDir);
+    var ext = extname && extname.substr(1);
+    if (ext && imgExts.indexOf(ext) > -1) {
+      items.push({ url: 'http://localhost:' + (port - 1) + '/' + path.basename(fileDir),
+        width: probe.sync(fs.readFileSync(fileDir)).width });
+    }
+  }).on('end', function () {
+    if (items.length) {
+      res.writeHead(200, {'Content-Type': jsonContentType});
+      res.end(JSON.stringify(items));
+    } else {
+      res.writeHead(404, {'Content-Type': 'text/plain'});
+      res.end('Not found');
+    }
+  })
+}
+
+http.createServer(function (request, response) {
+  var parsedUrl = url.parse(request.url, true);
+  var query = parsedUrl.query;
+
+  if (query.joke == '1') {
+    handleJoke1(response);
+  }
+  if (query.joke == '2') {
+    handleJoke2(response);
+  }
+}).listen(port);
+
+var file = new static.Server(local2, { cache: 72, headers: {'X-Hello':'World!'} });
+http.createServer(function (request, response) {
+  request.addListener('end', function () {
+    file.serve(request, response, function (err, res) {
+      if (err) { // An error as occured
+        console.error("> Error serving " + request.url + " - " + err.message);
+        response.writeHead(err.status, err.headers);
+        response.end();
+      } else {
+        console.log("> " + request.url + " - " + res.message);
+      }
+    });
+  }).resume();
+}).listen(port - 1);
+
+console.log('> main server running on port ' + port);
+console.log('> static server running on port ' + (port - 1));
+
